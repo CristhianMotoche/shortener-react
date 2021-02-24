9ae7e8405763e47ad1571894d937f23d43887a20
CristhianMotoche <cristhian.motoche@gmail.com>
2021-02-23 21:17:03 -0500

    Test that we can submit form and get a result

--- src/components/ShortLink.tsx
+++ src/components/ShortLink.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Formik, Form, Field } from "formik";
 
 interface Short {
@@ -9,25 +9,37 @@ const init: Short = {
   word: ""
 }
 
-const onSubmit = (values: Short) => {
-  console.log(values);
-};
 
-const ShortLink = (): JSX.Element => (
-  <section>
-    <header>
-      <h1>Create a short link</h1>
-    </header>
-    <Formik initialValues={init} onSubmit={onSubmit}>
-      <Form>
-        <Field
-          name="word"
-          type="text"
-          placeholder="Text"
-        />
-      </Form>
-    </Formik>
-  </section>
-);
+const ShortLink = (): JSX.Element => {
+  const [shortLink, setShortLink] = useState<string>();
+  const onSubmit = (values: Short) => {
+    setShortLink("123456");
+  };
+
+  return (
+    <section>
+      <header>
+        <h1>Create a short link</h1>
+      </header>
+      <div>
+        {shortLink && `Your short link is: ${shortLink}`}
+      </div>
+      <Formik initialValues={init} onSubmit={onSubmit}>
+        <Form>
+          <Field
+            name="word"
+            type="text"
+            placeholder="Text"
+          />
+          <input
+            name="sumbit"
+            type="submit"
+            value="Submit"
+          />
+        </Form>
+      </Formik>
+    </section>
+  );
+};
 
 export default ShortLink;
--- tests/ShortLink.test.tsx
+++ tests/ShortLink.test.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { render, fireEvent } from "@testing-library/react";
+import { render, fireEvent, waitFor } from "@testing-library/react";
 
 import ShortLink from "../src/components/ShortLink";
 
@@ -8,6 +8,21 @@ describe("ShortLink", () => {
     const { getByPlaceholderText } = render(<ShortLink />);
 
     const inputTest = getByPlaceholderText("Text");
-    fireEvent.change(inputTest, { target: { value: "Foo" } })
+    fireEvent.change(inputTest, { target: { value: "Foo" } });
+  });
+
+  describe("when clicking on submit button", () => {
+    describe("when the text is given", () => {
+      test("show the result", async () => {
+        const { getByPlaceholderText, getByText } = render(<ShortLink />);
+        const inputTest = getByPlaceholderText("Text");
+        const submitButton = getByText("Submit");
+
+        fireEvent.change(inputTest, { target: { value: "Foo" } });
+        fireEvent.click(submitButton);
+
+        await waitFor(() => getByText(/Your short link is: .{6}/));
+      });
+    });
   });
 });
